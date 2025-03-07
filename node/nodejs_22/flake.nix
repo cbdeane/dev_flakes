9159dff49aea7de54d5f82406faf3c77af66ea41
Charles Deane <30459123+cbdeane@users.noreply.github.com>
2025-03-07 09:38:53 -0500

    changed to defaultTemplate from default

--- node/nodejs_22/flake.nix
+++ node/nodejs_22/flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = let 
+    devShells.x86_64-linux.defaultTemplate = let 
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in 
       pkgs.mkShell {
