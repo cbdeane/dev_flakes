42d0b0ded29ebd8823583472544b68e642593a6d
Charles Deane <30459123+cbdeane@users.noreply.github.com>
2025-03-10 13:48:57 -0400

    finished python_ld_lib flake, will try to make this the default method for python development

--- python/python_default/flake.nix
+++ python/python_default/flake.nix
@@ -6,11 +6,43 @@
   };
 
   outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = let 
+    devShells.x86_64-linux.default = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pythonPackages = pkgs.python311Packages;
     in 
       pkgs.mkShell {
-        packages = [ pkgs.python3 pkgs.pipx];
+        name = "python-dev-shell";
+        venvDir = "./.venv";
+        packages = [ 
+
+          #packages to get python running in general
+          pythonPackages.python
+          pythonPackages.venvShellHook
+
+          #these are the dependencies that we want to add to PYTHONPATH in the venv
+          pythonPackages.numpy
+
+          # binary extensions that the pythonPackages may require
+          pkgs.taglib
+          pkgs.openssl
+          pkgs.git
+          pkgs.libxml2
+          pkgs.libxslt
+          pkgs.libzip
+          pkgs.zlib
+          pkgs.libz
+          pkgs.stdenv.cc.cc.lib
+        ];
+
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          pip install -r requirements.txt
+        '';
+
+        postShellHook = ''
+          # allow pip to install wheels
+          unset SOURCE_DATE_EPOCH
+        '';
       };
   };
 }
