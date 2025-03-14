5eeaf567b31670bd5134b834d03a8b5cb2b66d98
Charles Deane <30459123+cbdeane@users.noreply.github.com>
2025-03-14 16:27:03 -0400

    specified dotnet version

--- csharp/dotnet_9_template/flake.nix
+++ csharp/dotnet_9_template/flake.nix
@@ -3,7 +3,7 @@
 
     templates.default = {
       description = "Developer shell flake for dotnet 9.0";
-      path = ./dotnet;
+      path = ./dotnet_9;
       welcomeText = ''
         Initialized dotnet developer shell
       '';
